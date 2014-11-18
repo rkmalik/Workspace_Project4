e7c5d3bd912f210e9857366856f77149acf15cd5
Rohit Kumar Malik <rkmalik@ufl.edu>
2014-11-18 00:04:15 -0500

    Project4 Code Including Common
    This will require the akka library to be included in the Build Path

---
+++ Common/src/common/Common.scala
@@ -0,0 +1,21 @@
+package common
+
+import java.util.ArrayList
+
+case class TweetTransmit (clientid: Int, msg: String)
+case class UserData (clientid: Int)
+case class Acknowledge (msg: String)
+case class 	Message (msg : String)
+
+
+case class 	MineBitcoinsActor(start : Int, nrofElements: Int)
+    case class 	MineBitcoinsWithinRange(start : Int, nrofElements: Int, pattern : StringBuffer)
+    case class  ServerToClient (msg : String)
+    case class  ClientToServer (msg : String)
+    case class  ServerToClientReq (start : Int, nrofElements: Int, pattern : StringBuffer)
+	case class  ClientToServerResp (result : Int, bitCoins : String)
+	case class 	Outputdetail (result : Int, bitCoins : String)
+	case class 	ServerWorkerResp (result : Int, bitCoins : String)
+    case object MineBitcoins
+
+    
---
+++ readme.txt
@@ -0,0 +1,3 @@
+
+1. This Project will require the all the Akka library Jars to be included.
+2. common.jar contains all the common case class declaration used by the client and the server.
